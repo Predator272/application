08c06dc2a46cf146ae5320a0c10c9cb303f01fe3
Predator272 <87614167+Predator272@users.noreply.github.com>
2022-07-09 20:59:59 +0300

    435334534

--- views/user/index.php
+++ views/user/index.php
@@ -6,9 +6,30 @@ use yii\bootstrap4\Html;
 $this->title = 'Профиль';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<h1>user/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<div class="site-profile">
+    <div class="img">
+        <div class="placeholder">$avatar</div><!--потом заменить на авы через бд-->
+    </div>
+    <div class="info">
+        <h4>$name</h4>
+        <div class="description">
+            <strong>ID:</strong> $id
+        </div>
+    </div>
+</div>
+<style>
+    .placeholder{
+        padding: 100px 85px;
+        float: left;
+        -webkit-border-radius: 100px;
+        -moz-border-radius: 100px;
+        border-radius: 9999999px;
+        background: #d9d9d9;
+    }
+    .info{
+        float: right;
+        padding-left: 35vw;
+        background: #8e8e8e;
+    }
+</style>
